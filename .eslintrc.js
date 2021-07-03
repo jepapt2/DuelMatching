ea00390fc1009543376f115b70d16cd24520ac66
jepapt2 <j.webprogram@gmail.com>
2021-07-03 22:24:13 +0900

    stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
     'airbnb/hooks',
-    'plugin:import/errors', 
+    'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
@@ -28,86 +28,92 @@ module.exports = {
     '@typescript-eslint',
     'import',
     'jsx-a11y',
+    'prefer-arrow',
     'react',
     'react-hooks',
   ],
   root: true,
   rules: {
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': [
-        'error',
-      ],
-      'lines-between-class-members': [ 
-        'error',
-        'always',
-        {
-          exceptAfterSingleLine: true 
-        }
-      ], 
-      'no-void': [
-        'error',
-        {
-          allowAsStatement: true, 
-        },
-      ], 
-      'padding-line-between-statements': [
-        'error', 
-        {
-          blankLine: 'always',
-          prev: '*',
-          next: 'return',
-        }, 
-      ],
-      '@typescript-eslint/no-unused-vars': [ 
-        'error',
-        {
-          'vars': 'all',
-          'args': 'after-used',
-          'argsIgnorePattern': '_',
-          'ignoreRestSiblings': false,
-          'varsIgnorePattern': '_',
-        },
-      ],
-      'import/extensions': [ 
-        'error',
-        'ignorePackages',
-        {
-          js: 'never',
-          jsx: 'never',
-          ts: 'never',
-          tsx: 'never',
-        }
-      ],
-      'react/jsx-filename-extension': [
-        'error',
-        {
-          extensions: ['.jsx', '.tsx']
-        }
-      ],
-      'react/jsx-props-no-spreading': [
-        'error', 
-        {
-          html: 'enforce',
-          custom: 'enforce',
-          explicitSpread: 'ignore',
-        },
-      ],
-      'react/react-in-jsx-scope': 'off', 
-    },
-    overrides: [ 
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'lines-between-class-members': [
+      'error',
+      'always',
       {
-        'files': ['*.tsx'],
-        'rules': {
-          'react/prop-types': 'off',
-        },
+        exceptAfterSingleLine: true,
       },
     ],
-    settings: { 
-      'import/resolver': {
-        node: {
-        paths: ['src'],
-        },
+    'no-void': [
+      'error',
+      {
+        allowAsStatement: true,
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '_',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '_',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
       },
     },
-  }
-
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  'prefer-arrow/prefer-arrow-functions': [
+    'error',
+    {
+      disallowPrototype: true,
+      singleReturnOnly: false,
+      classPropertiesAllowed: false,
+    },
+  ],
+};
